62841ebf97ae7081871f36a265d8fb229a0afe13
Joshua Kenney <joshuagkenney@gmail.com>
2020-08-20 18:47:19 -0400

    ready for testing

--- exp/var.js
+++ exp/var.js
@@ -17,5 +17,115 @@ let stim_array = [];
 
 let main_stimuli = [];
     for (let i =0;i<stim_array.length;i++) {
-        main_stimuli.push({trial: stim_array[i], data: {test_part: 'experiment', correct_response: ''}},)
+        main_stimuli.push({stimulus: stim_array[i], data: {test_part: 'experiment', correct_response: ''}},)
 }
+
+// let main_stimuli = [
+//     {stimulus: stim_array[0], 
+//     data: {test_part: 'experiment', 
+//     correct_response: ','}},
+
+//     {stimulus: stim_array[1], 
+//     data: {test_part: 'experiment', 
+//     correct_response: ','}},  
+
+//     {stimulus: stim_array[2], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}}, 
+
+//     {stimulus: stim_array[3], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[4], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[5], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[6], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[7], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[8], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[9], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[10], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[11], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[12], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[13], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[14], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[15], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[16], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[17], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[18], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[19], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[20], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[21], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[22], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[23], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[24], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[25], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}},
+
+//     {stimulus: stim_array[26], 
+//     data: {test_part: 'experiment', 
+//     correct_response: '.'}}
+// ]
