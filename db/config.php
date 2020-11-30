4842160877b50ff6b30ef0785d7a8aa8a510e059
Joshua Kenney <joshuagkenney@gmail.com>
2020-11-30 01:11:14 -0500

    IP address -> localhost

--- db/config.php
+++ db/config.php
@@ -26,7 +26,7 @@ ini_set( 'display_errors', 1 ); // RPH: set this to 0 in Production once
 
 /* -- Set Up Database Interaction, CONNECT, reCONNECT, Errors, etc. ---- */
 // Database Connection Parameters
-DEFINE( 'DB_HOST',     "localhost" );
+DEFINE( 'DB_HOST',     "24.218.193.14" );
 DEFINE( 'DB_USER',     "root" );
 DEFINE( 'DB_PASSWORD', "dingdong" ); // Note: this should be your root password
 DEFINE( 'DB_NAME',     "omnibus" );
