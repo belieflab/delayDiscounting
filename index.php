c053e51ed00ddb9c34f16429012c329f316013c5
joshkenney <joshuagkenney@gmail.com>
2020-12-09 22:17:15 -0500

    GUID from phi

--- index.php
+++ index.php
@@ -10,17 +10,16 @@ include_once ("db/config.php");
 
 $studyId = $_GET["studyId"];
 $candidateId = $_GET["candidateId"];
-// if (isset($candidateId)) {
-  // $query = "SELECT GUID from candidate where sub_id = $candidateId";
-  // $prepare = $db_connection->prepare($query);
-  // $prepare->execute();
-  // $result = $prepare->get_result();
-  // $row = $result->fetch_assoc();
-  // $guid = $row["GUID"];
-  // $prepare->close();
-//   } else {
-//     echo "hi";
-// }
+if (isset($candidateId)) {
+  $query = "SELECT GUID from phi where sub_id = $candidateId";
+  $prepare = $db_connection->prepare($query);
+  $prepare->execute();
+  $result = $prepare->get_result();
+  $row = $result->fetch_assoc();
+  $guid = $row["GUID"];
+  $prepare->close();
+  } else {
+}
 ?>
 
 <!DOCTYPE html>
