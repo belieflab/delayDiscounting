ff91f4f2c3066f44aec5d839ed34351fb69a2c53
joshkenney <joshuagkenney@gmail.com>
2021-01-09 19:20:37 -0500

    jQuery -> 3.5.1, jQuery API refactor, bug fixes

--- exp/timeline.js
+++ exp/timeline.js
@@ -117,6 +117,23 @@ let end = {
   // trial_duration: 60000,
 };
 
+/* define procedures trial */
+
+let instructions = {
+  timeline: [welcome, instr_1, instr_2, instr_3, instr_4, instr_5]
+};
+
+let procedures = {
+  timeline: [fixation, trial],
+  timeline_variables: main_stimuli,
+  randomize_order: false,
+};
+
+/* main */
+timeline.push(instructions);
+timeline.push(procedures);
+timeline.push(save_data);
+timeline.push(end);
 
 
 
--- index.php
+++ index.php
@@ -32,6 +32,7 @@ $ageInMonths = $_GET["interview_age"];
   <head>
     <title>Delay Discounting</title>
     <script src="db/validate.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <script src="jsPsych/jspsych.js"></script>
     <script src="jsPsych/plugins/jspsych-html-keyboard-response.js"></script>
     <script src="jsPsych/plugins/jspsych-image-keyboard-response.js"></script>
@@ -39,13 +40,21 @@ $ageInMonths = $_GET["interview_age"];
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body id='unload' onbeforeunload="return areYouSure()" style="background-color: white;">  
-    <?php include_once "include/nda.php"?>
+    <?php
+      if ($db_connection_status == true) {
+        include_once "include/nda.php";
+        // echo'<br>';
+        // echo'connected';
+      } else if ($db_connection_status == false) {
+        include_once "include/intake.php";
+        // echo'<br>';
+        // echo'not connected';
+      }
+    ?>
   </body>
   <footer>
-    <script type="text/javascript" src="//code.jquery.com/jquery-git.js"></script>
     <script src="exp/fn.js"></script>
     <script src="exp/var.js"></script>
-    <!-- <script src="exp/timeline.js"></script> -->
     <script type="text/javascript">
     let feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/dd.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";
     let GUID = "<?php echo $subjectKey?>";
