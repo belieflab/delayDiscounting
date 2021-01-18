2098e8502f751dc5fe54e05084d223f5f32977fc
joshkenney <joshuagkenney@gmail.com>
2021-01-17 19:02:03 -0500

    linted and refactored

--- exp/timeline.js
+++ exp/timeline.js
@@ -4,55 +4,54 @@ let timeline = [];
 let welcome = {
     type: "html-keyboard-response",
     stimulus: '<h3 style="color:blue;">Welcome to the experiment!</h3>'+
-    '<p><i>Press any key to begin.</i></p>',
-}
+    '<p><i>Press any key to begin.</i></p>'
+};
 
 let instr_1 = {
   type:"html-keyboard-response",
   stimulus:'<p>In the following task, you will be asked to pretend to choose between earning either:</p>'+
   '<p><strong>a little money now</strong>&nbsp&nbsp&nbsp&nbsp&nbsp <i>or</i> &nbsp&nbsp&nbsp&nbsp&nbsp<strong>a little more money, later</strong></p>'+
   '<p><i>Press the spacebar to continue.</i></p>',
-  choices: [32],
-}
+  choices: [32]
+};
 
 let instr_2 = {
   type:"html-keyboard-response",
   stimulus:'<p>To make your selection on each trial:<br></p>'+
   '<p><strong>press the <u>1</u> key</strong>&nbsp&nbsp&nbsp&nbsp&nbsp <i>or</i> &nbsp&nbsp&nbsp&nbsp&nbsp <strong>press the <u>0</u> key</strong><br>'+
   '<p><i>Press either of the response keys to continue.</i></p>',
-  choices: ['1', '0'],
-}
+  choices: ['1', '0']
+};
 
 let instr_3 = {
   type:"html-keyboard-response",
   stimulus:'<p>There are no right or wrong answers.<br></p>'+
   '<p> Please take your time, and respond as truthfully as you can.</p>'+
   '<p><i>Press the spacebar to continue.</i></p>',
-  choices: [32],
-}
+  choices: [32]
+};
 
 let instr_4 = {
   type:"html-keyboard-response",
   stimulus: '<p style="color:black;"> Get Ready...</p>',
-  
-  trial_duration: 2000,
-}
+  trial_duration: 2000
+};
 
 let instr_5 = {
   type:"html-keyboard-response",
   stimulus:'<p style="color:black; font-size:60px;" id="countdown"></p>',
   trial_duration: 5200,
   on_start: countdown(),
-  response_ends_trial: false,
-  }
+  response_ends_trial: false
+  };
 
 
 let fixation = {
   type: 'html-keyboard-response',
   stimulus: '<div style="font-size:60px;">+</div>',
   choices: jsPsych.NO_KEYS,
-  trial_duration: 1000,
-}
+  trial_duration: 1000
+};
 
 let trial = {
   type: "html-keyboard-response",
@@ -60,6 +59,7 @@ let trial = {
   choices: ['1', '0'],
   data: jsPsych.timelineVariable('data'),
   on_finish: function(data){
+    'use strict';
     data.subjectkey = GUID;
     data.src_subject_id = workerId;
     data.site = siteNumber;
@@ -76,32 +76,33 @@ let trial = {
       data.response = 'sir';
     }
   }
-}
+};
 
 let save_data = {
   type: "html-keyboard-response",
   stimulus: "<p>Data saving...</p>"+
   '<div class="sk-cube-grid">'+
-'<div class="sk-cube sk-cube1"></div>'+
-'<div class="sk-cube sk-cube2"></div>'+
-'<div class="sk-cube sk-cube3"></div>'+
-'<div class="sk-cube sk-cube4"></div>'+
-'<div class="sk-cube sk-cube5"></div>'+
-'<div class="sk-cube sk-cube6"></div>'+
-'<div class="sk-cube sk-cube7"></div>'+
-'<div class="sk-cube sk-cube8"></div>'+
-'<div class="sk-cube sk-cube9"></div>'+
-'</div>'+
+  '<div class="sk-cube sk-cube1"></div>'+
+  '<div class="sk-cube sk-cube2"></div>'+
+  '<div class="sk-cube sk-cube3"></div>'+
+  '<div class="sk-cube sk-cube4"></div>'+
+  '<div class="sk-cube sk-cube5"></div>'+
+  '<div class="sk-cube sk-cube6"></div>'+
+  '<div class="sk-cube sk-cube7"></div>'+
+  '<div class="sk-cube sk-cube8"></div>'+
+  '<div class="sk-cube sk-cube9"></div>'+
+  '</div>'+
   "<p>Do not close this window until the text dissapears.</p>",
 
   choices: jsPsych.NO_KEYS,
   trial_duration: 5000,
   on_finish: function(){
+    'use strict';
     saveData("dd_" + workerId, jsPsych.data.get().csv());
-    document.getElementById("unload").onbeforeunload='';
-    $(document).ready(function(){
-    $("body").addClass("showCursor"); // returns cursor functionality
-});
+      document.getElementById("unload").onbeforeunload='';
+      $(document).ready(function(){
+      $("body").addClass("showCursor"); // returns cursor functionality
+    });
   }
 };
 
@@ -114,26 +115,10 @@ let end = {
   // "<p>Please wait for the experimenter to continue.</p>"+
   "<p><i>You may now close the expriment window at anytime.</i></p>",
   choices: jsPsych.NO_KEYS,
-  // trial_duration: 60000,
 };
 
-/* define procedures trial */
-
-let instructions = {
-  timeline: [welcome, instr_1, instr_2, instr_3, instr_4, instr_5]
-};
-
-let procedures = {
-  timeline: [fixation, trial],
-  timeline_variables: main_stimuli,
-  randomize_order: false,
-};
-
-/* main */
-timeline.push(instructions);
-timeline.push(procedures);
-timeline.push(save_data);
-timeline.push(end);
+// call main
+$.getScript("exp/main.js");
 
 
 
