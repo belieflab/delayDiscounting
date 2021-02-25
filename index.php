a8fe901dad320825a47a9af01ebf712b293f94f2
joshkenney <joshuagkenney@gmail.com>
2021-02-25 11:09:51 -0500

    adjusted error in feedback link

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
         siteNumber = "<?php echo $institutionAlias?>";
         ageAtAssessment = "<?php echo $ageInMonths?>";
         groupStatus = "<?php echo $groupStatus?>";
-        feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/kamin.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";
+        feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/dd.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";
       }
     </script>
   </footer>
