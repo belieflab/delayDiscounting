fbe7eee4a82d066bcf697f5e30a0695fe25df28d
root <root@ip-10-5-120-93.its.yale.internal>
2024-05-28 16:47:37 +0000

    updated db/config.php and index.php to work with php8

--- index.php
+++ index.php
@@ -16,21 +16,25 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body id='unload' onbeforeunload="return areYouSure()" style="background-color: white;">  
-    <?php
-      if ($db_connection_status == true) {
+  <?php
+      if (isset($_GET["src_subject_id"])) {
         include_once "include/nda.php";
         // echo'<br>';
         // echo'connected';
-      } else if ($db_connection_status == false) {
-        include_once "include/intake.php";
+      } else if (isset($_GET["workerId"])) {
+        include_once "include/consent.php";
         // echo'<br>';
         // echo'not connected';
+      } else {
+        include_once "include/intake.php";
       }
     ?>
   </body>
   <footer>
-    <script src="exp/fn.js"></script>
-    <script src="exp/var.js"></script>
+  <script type="text/javascript" src="exp/conf.js"></script>
+  <script defer type="text/javascript" src="exp/var.js"></script>
+  <script type="text/javascript" src="exp/fn.js"></script>
+
     <script type="text/javascript">
       // declare NDA required variables
       let GUID;
@@ -39,25 +43,18 @@
       let siteNumber;
       let ageAtAssessment;
       let groupStatus;
+      let visit;
       let feedbackLink;
 
-      if (db_connection === false) {
-        GUID = "";
-        subjectID = "";
-        sexAtBirth = "";
-        siteNumber = "";
-        ageAtAssessment = "";
-        groupStatus = "";
-        feedbackLink = "";
-      } else if (db_connection === true) {
+
         GUID = "<?php echo $subjectKey?>";
         subjectID = "<?php echo $consortId?>";
         sexAtBirth = "<?php echo $sexAtBirth?>";
         siteNumber = "<?php echo $institutionAlias?>";
         ageAtAssessment = "<?php echo $ageInMonths?>";
         groupStatus = "<?php echo $groupStatus?>";
-        feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/dd.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";
-      }
-    </script>
+        visit = "<?php echo $visit?>";
+feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/kamin.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";    
+</script>
   </footer>
 </html>
